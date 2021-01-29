fc4fa8eacebfe4aebcb42898aeb85c3335b38b40
Ahmad Taha <ataha@netways.com>
2021-01-29 23:11:14 +0200

    💄⚡️ finshing FirstChallenge design

--- firstChallenge/index.tsx
+++ firstChallenge/index.tsx
@@ -1,47 +1,90 @@
 import * as React from 'react';
 import styled from 'styled-components/native';
 import LinearGradient from 'react-native-linear-gradient';
-import {ImageRequireSource, StatusBar} from 'react-native';
+import {ImageRequireSource, StatusBar, View} from 'react-native';
 
 const girlImage = require('./assets/images/toyFaces_girl.png') as ImageRequireSource;
 const boyImage = require('./assets/images/toyFaces_boy.png') as ImageRequireSource;
+const logo = require('./assets/images/logo.png') as ImageRequireSource;
 
-export interface FirstChallengeProps {}
+// colors
+const Colors = {
+  primary: '#FF4B3A',
+  secondary: '#FF470B',
+  white: '#fff',
+  transparent: '#00000000',
+};
 
-const FirstChallenge: React.FC<FirstChallengeProps> = () => {
+const FirstChallenge = () => {
   return (
     <>
-      <StatusBar backgroundColor="#FF4B3A" barStyle="light-content" />
-      <Container colors={['#FF4B3A', '#FF470B']}>
-        <LogoContainer></LogoContainer>
-        <Title>Food for Everyone</Title>
+      <StatusBar backgroundColor={Colors.primary} barStyle="light-content" />
+      <Background>
+        <Container>
+          <View>
+            <LogoContainer>
+              <Logo source={logo} />
+            </LogoContainer>
+            <Title>Food for Everyone</Title>
+          </View>
 
-        <BoyImage source={boyImage} style={{resizeMode: 'contain'}} />
-        <GradientLayerOverBoy colors={['#FF4B3A00', '#FF470B', '#FF470B']} />
-        <GirlImage source={girlImage} style={{resizeMode: 'contain'}} />
-        <GradientLayerOverGirl colors={['#FF4B3A00', '#FF470B', '#FF470B']} />
-      </Container>
+          <Button>
+            <ButtonText>Get Started</ButtonText>
+          </Button>
+        </Container>
+        <BoyView>
+          <BoyImage source={boyImage} style={{resizeMode: 'contain'}} />
+          <GradientLayerOverBoy
+            colors={[Colors.transparent, Colors.secondary]}
+          />
+        </BoyView>
+        <GirlView>
+          <GirlImage source={girlImage} style={{resizeMode: 'contain'}} />
+          <GradientLayerOverGirl
+            colors={[Colors.transparent, Colors.secondary]}
+          />
+        </GirlView>
+        <GradientLayerBackground
+          colors={[Colors.transparent, Colors.secondary]}
+        />
+      </Background>
     </>
   );
 };
 
 export default FirstChallenge;
 
-const Container = styled(LinearGradient)`
+// Background
+const Background = styled.View`
+  background-color: #ff4b3a;
   display: flex;
   flex: 1;
-  justify-content: flex-start;
-  align-items: flex-start;
-  background-color: #7163df;
-  padding: 50px;
   position: relative;
 `;
 
+// Container
+const Container = styled.View`
+  display: flex;
+  flex: 1;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 50px 50px 30px 50px;
+`;
+
+// Header
 const LogoContainer = styled.View`
-  width: 100px;
-  height: 100px;
+  width: 70px;
+  height: 70px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   border-radius: 50px;
   background-color: #fff;
+`;
+
+const Logo = styled.Image`
+  width: 50px;
+  height: 50px;
 `;
 
 const Title = styled.Text`
@@ -51,36 +94,79 @@ const Title = styled.Text`
   text-align: left;
 `;
 
-const GirlImage = styled.Image`
-  height: 450px;
-  width: 350px;
+const GradientLayerBackground = styled(LinearGradient)`
+  height: 50%;
+  width: 100%;
+  position: absolute;
+  z-index: 1;
+  bottom: 15%;
+`;
+
+// Girl Image
+const GirlView = styled.View`
+  height: 50%;
+  width: 80%;
   transform: rotate(-3.1deg);
   position: absolute;
-  left: -130px;
-  bottom: 100px;
+  left: -27%;
+  bottom: 15%;
+  z-index: 2;
 `;
 
-const BoyImage = styled.Image`
-  height: 300px;
-  width: 300px;
-  transform: rotate(8.6deg);
-  position: absolute;
-  right: -75px;
-  bottom: 150px;
-`;
-
-const GradientLayerOverBoy = styled(LinearGradient)`
-  width: 110%;
-  height: 200px;
-  position: absolute;
-  bottom: 70px;
-  right: 0;
+const GirlImage = styled.Image`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  z-index: 1;
 `;
 
 const GradientLayerOverGirl = styled(LinearGradient)`
-  width: 70%;
-  height: 200px;
+  width: 96%;
+  height: 50%;
   position: absolute;
-  bottom: 70px;
-  left: 0;
+  z-index: 2;
+  bottom: 0;
+`;
+
+// Boy Image
+const BoyView = styled.View`
+  height: 35%;
+  width: 100%;
+  transform: rotate(8.6deg);
+  position: absolute;
+  right: -33%;
+  bottom: 20%;
+  z-index: 2;
+`;
+
+const BoyImage = styled.Image`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  z-index: 1;
+`;
+
+const GradientLayerOverBoy = styled(LinearGradient)`
+  width: 100%;
+  height: 30%;
+  position: absolute;
+  z-index: 2;
+  bottom: 0;
+`;
+
+// Button
+const Button = styled.TouchableOpacity`
+  width: 100%;
+  background-color: #fff;
+  border-radius: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ButtonText = styled.Text`
+  color: #ff4b3a;
+  font-size: 16px;
+  font-family: 'SFProText-Regular-1';
+  margin: 20px;
 `;
